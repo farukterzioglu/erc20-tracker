a1eff1e318462568e2ea1dcebf7d91e885e08777
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2019-08-03 00:30:48 +0300

    Invoke erc20 contract through unit tests

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## A Minimalist ERC-20 Tracker  
 
 `docker run -d --hostname my-rabbit --name some-rabbit -p 4369:4369 -p 5671:5671 -p 5672:5672 -p 15672:15672 rabbitmq:3-management`
-
+Navigate to 'http://localhost:15672/#/'  
 
 Samples;   
 ```
@@ -12,13 +12,18 @@ docker build -t erc20-tracker .
 $ docker run -e CONTRACTADDRESS=0xdac17f958d2ee523a2206206994597c13d831ec7 erc20-tracker 
 
 $ docker run \
-    -e CONTRACTADDRESS=0xdac17f958d2ee523a2206206994597c13d831ec7 \
-    -e ADDRESS=0xe7483f9dca3b823e87a6e0d6c3c8ee15aa651799 \
+    -e CONTRACTADDRESS:0=0xdac17f958d2ee523a2206206994597c13d831ec7 \
+    -e TRACKEDADDRESSES:0=0xde769dda17eD7d2178a5646D279382d0CaEC8079 \
     erc20-tracker
 
 $ docker run \
-    -e CONTRACTADDRESS=0xdac17f958d2ee523a2206206994597c13d831ec7 \
-    -e ADDRESS=0xe7483f9dca3b823e87a6e0d6c3c8ee15aa651799 \
-    -e ZEROMQURL=127.0.0.1:5556 \
+    -e NodeUrl=http://localhost:7545 \
+    -e CONTRACTADDRESS:0=0xdac17f958d2ee523a2206206994597c13d831ec7 \
+    -e TRACKEDADDRESSES:0=0xde769dda17eD7d2178a5646D279382d0CaEC8079 \
+    -e TRACKEDADDRESSES:1=0xD5eaddD9462D2138389d3Ca281c3a10c952267E5 \
+    -e RABBITMQHOSTNAME=localhost \
+    -e RABBITMQUSERNAME=guest \
+    -e RABBITMQPASSWORD=guest \
+    -e RABBITMQEXCHANGENAME=Erc20Transactions \
     erc20-tracker
 ```
--- erc20-tracker-test/IntegrationTests.cs
+++ erc20-tracker-test/IntegrationTests.cs
@@ -1,4 +1,7 @@
+using System.Numerics;
 using System.Threading.Tasks;
+using erc20_tracker_test;
+using Nethereum.HdWallet;
 using Nethereum.StandardTokenEIP20.ContractDefinition;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
@@ -9,27 +12,55 @@ namespace Tests
     public class IntegrationTests
     {
         string _contractAddress;
+        string _privateKey;
+        string _receiver;
+
         Account _account;
         Web3 _web3;
 
-        [SetUp]
-        public void Setup()
-        {
-            string url = "http://localhost:7545";
-            string privateKey = "a9ac3598209f080e027c60bbee4d83dadb2bb738503c9fd974dc1bb55891ef05";
-
-            _account = new Account(privateKey);
-            _web3 = new Web3(_account, url);
-
-            _contractAddress = "0xAbC0ecdaD3c1EFc21a6857F288def37CD5439d80";
+        [OneTimeSetUp]
+        public async Task SetupOnce()
+        {   
+            SetAccount();
+            _web3 = new Web3(_account, "http://localhost:7545");
+            await DeployContract();
         }
+
+        [Test]
+        public void SetAccount() 
+        {
+            string seed = "hundred couple soup flash wait scorpion night donate manual mean flee ability";
+            string pass = "";
+
+            var wallet = new Wallet(seed, pass);
+
+            _account = wallet.GetAccount(0);
+            _privateKey = _account.PrivateKey;
+            _receiver = wallet.GetAccount(1).Address;
+        }
+
+        [Test]
+        public async Task DeployContract() 
+        {
+            var deploymentMessage = new TetherTokenDeployment { TotalSupply = 100000 };
+            var deploymentHandler = _web3.Eth.GetContractDeploymentHandler<TetherTokenDeployment>();
+            var transactionReceipt = await deploymentHandler.SendRequestAndWaitForReceiptAsync(deploymentMessage);
+            
+            _contractAddress = transactionReceipt.ContractAddress;
+        }
+        
 
         [Test]
         public async Task Transfer()
         {
             var transferHandler = _web3.Eth.GetContractTransactionHandler<TransferFunction>();
-            var transfer = new TransferFunction() { To = "0xde769dda17eD7d2178a5646D279382d0CaEC8079", Value = 75 };
-            await transferHandler.SendRequestAndWaitForReceiptAsync("0xAbC0ecdaD3c1EFc21a6857F288def37CD5439d80", transfer);
+            var transfer = new TransferFunction() { To = _receiver, Value = 100 };
+            await transferHandler.SendRequestAndWaitForReceiptAsync(_contractAddress, transfer);
+
+            var balanceHandler = _web3.Eth.GetContractQueryHandler<BalanceOfFunction>();
+            var currentBalance = await balanceHandler.QueryAsync<BigInteger>(_contractAddress, new BalanceOfFunction() { Owner = _receiver });
+            
+            Assert.AreEqual((BigInteger)100, currentBalance);
         }
     }
 }
